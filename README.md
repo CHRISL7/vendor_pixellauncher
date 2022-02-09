7e2a9c4cb457ef555d3c32b1434ac1b00727880b
andrej <andrejw39@gmail.com>
2022-02-09 17:06:01 +0100

    PixelLauncher: Initial Release

---
+++ README.md
@@ -0,0 +1,17 @@
+# Prebuilt PixelLauncher with all needed packages
+
+## Disclaimer
+- All credits go to the [saitama_96](https://t.me/saitama_96) & [MrSluffy_69](https://t.me/MrSluffy_69).
+
+## Getting started
+**1. Add this repository to your AOSP build root's `vendor/pixellauncher`**
+
+**2. Import vendor/pixellauncher/wenmonet.mk**
+
+`$(call inherit-product-if-exists, vendor/pixellauncher/wenmonet.mk)`
+
+**3. Remove existing launcher from the build**
+
+Not needed, it will ovewrrite almost all Launchers that exist but if needed more Launchers can be added to be overwritten by adding the name in PixelLauncher/Android.mk:27 LOCAL_OVERRIDES_PACKAGES := LauncherName
+
+**4. Build Android**# vendor_pixellauncher
---
+++ wenmonet.mk
@@ -0,0 +1,26 @@
+##############################################################
+##															##
+##    ███╗░░░███╗██╗░░██╗░█████╗░██╗░█████╗░███╗░░██╗		##
+##    ████╗░████║╚██╗██╔╝██╔══██╗██║██╔══██╗████╗░██║		##
+##    ██╔████╔██║░╚███╔╝░██║░░╚═╝██║██║░░██║██╔██╗██║		##
+##    ██║╚██╔╝██║░██╔██╗░██║░░██╗██║██║░░██║██║╚████║		##
+##    ██║░╚═╝░██║██╔╝╚██╗╚█████╔╝██║╚█████╔╝██║░╚███║		##
+##    ╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░╚════╝░╚═╝░░╚══╝		##
+##															##
+##############################################################
+
+
+# PixelLauncher & other necessary prebuilts 
+PRODUCT_PACKAGES += \
+	PixelLauncher \
+	GoogleWallpaperPicker \
+    PixelThemesStub
+    
+
+# These are all nesesary sysconfig & permission files fo NexusLauncher & WallpaperPicker
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/PixelLauncher/etc/preinstalled-packages-platform-overlays.xml:system/etc/sysconfig/preinstalled-packages-platform-overlays.xml \
+    $(LOCAL_PATH)/PixelLauncher/etc/google-hiddenapi-package-whitelist.xml:system/etc/sysconfig/google-hiddenapi-package-whitelist.xml\
+    $(LOCAL_PATH)/PixelLauncher/etc/privapp-permissions-pixel-launcher.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pixel-launcher.xml \
+    $(LOCAL_PATH)/PixelLauncher/etc/launcher-hiddenapi-package-whitelist.xml:system/etc/sysconfig/launcher-hiddenapi-package-whitelist.xml \
+	$(LOCAL_PATH)/GoogleWallpaperPicker/etc/privapp-permissions-com.google.android.apps.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.google.android.apps.wallpaper.xml
